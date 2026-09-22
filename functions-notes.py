dadf02658812426b6cc42e7af611897b39955ee2
Yunice Fariñas <yunice@Yunices-MacBook-Pro.local>
2026-09-22 17:44:20 -0400

    Add assignment files

--- functions-notes.py
+++ functions-notes.py
@@ -20,8 +20,5 @@ var2 = 34
 myAnswer = sumItUp (var1, var2) #This calls the function with two arguments for the parameters for the func.
 print(myAnswer)
 
-
-
-
-
+#Function can have as many parameter as it wants. Each parameter is separated by a comma.
 
