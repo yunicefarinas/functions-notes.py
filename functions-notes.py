b1c903c6776dde0ec0059de12d497b050c33890d
Yunice Fariñas <yunice@Yunices-MacBook-Pro.local>
2026-09-22 17:03:14 -0400

    Add Python scripts and notes

---
+++ functions-notes.py
@@ -0,0 +1,27 @@
+#function with no parameters and does not return a value
+def printMessage():
+    print("I am inside of a function and I don't return a value")
+    print("All of my capabilities are performed inside the function only")
+
+def returnMyMessage():
+    return "I am a returning value"
+
+#this function has two parameters and returns a value
+def sumItUp(value1, value2):
+    answer = value1 + value2
+    return answer
+
+#call a function by its name to activate it.
+#printMessage()
+#message = returnMyMessage()
+#print(message)
+var1 = 33
+var2 = 34
+myAnswer = sumItUp (var1, var2) #This calls the function with two arguments for the parameters for the func.
+print(myAnswer)
+
+
+
+
+
+
